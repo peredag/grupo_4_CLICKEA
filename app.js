be4ba524457aacf0ca601f443bd26388cfe87003
Jona <retratos.jonatan@gmail.com>
2021-12-02 18:09:11 -0300

    Realiza algunos ajustes en los css

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let path = require('path')
 app.use(express.static("public"));
 
 
-app.get('/', function(req, res) {res.sendFile(path.join(__dirname, "views/index.html"))})
+app.get('/', function(req, res) {res.sendFile(path.join(__dirname, "views/home.html"))})
 
 app.get('/productDetail', function(req, res) {res.sendFile(path.join(__dirname, "views/productDetail.html"))})
 
