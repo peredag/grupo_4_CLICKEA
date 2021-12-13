7c0c67c1ac026aff1dba6958b3718158883c9d1a
JonathanChallco <jonathanchallco21@gmail.com>
2021-12-13 17:35:57 -0300

    Correcciones

--- src/app.js
+++ src/app.js
@@ -13,16 +13,6 @@ app.use('/productos', rutasProductos);
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-
-
-app.get("/register", function (req, res) {
-  res.sendFile(path.join(__dirname, "views/register.ejs"));
-});
-
-app.get("/login", function (req, res) {
-  res.sendFile(path.join(__dirname, "views/login.ejs"));
-});
-
 app.listen(PORT, () =>
   console.log(
     `Servidor levantado en el puerto ${PORT} http://localhost:${PORT}`
